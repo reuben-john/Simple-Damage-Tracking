1ee1c3ce5324f0f2d96e483215528f82cbdb6e41
Reuben John <rjkoppy@gmail.com>
2018-06-06 17:20:06 -0400

    Set up basic list of order damages

---
+++ simple-damages-tracker/src/components/Index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="index container">
+    <div class="collection" v-for="loss in orderDamages" :key="loss.orderNumber">
+        <a class="collection-item">Order Number: {{ loss.orderNumber }}</a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index',
+  data() {
+    return {
+      orderDamages: [
+        {
+          date: '01/05/2018',
+          orderNumber: '5',
+          orderCost: '5.99',
+          shippingCost: '2.61',
+          shippingLoss: '2.61',
+          itemType: 'beauty',
+          itemCost: '1',
+          itemsLost: '2',
+          lossType: 'Bad Product'
+        },
+        {
+          date: '02/05/2018',
+          orderNumber: '25',
+          orderCost: '15.99',
+          shippingCost: '5.61',
+          shippingLoss: '3.61',
+          itemType: 'clothing',
+          itemCost: '4',
+          itemsLost: '0',
+          lossType: 'Returned Product'
+        },
+        {
+          date: '02/05/2018',
+          orderNumber: '15',
+          orderCost: '8.99',
+          shippingCost: '2.61',
+          shippingLoss: '2.61',
+          itemType: 'makeup',
+          itemCost: '0.3',
+          itemsLost: '1',
+          lossType: 'Returned Product'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.index {
+  margin: 20px auto;
+}
+.collection {
+  margin: 0 auto;
+}
+</style>
---
+++ simple-damages-tracker/src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="navbar">
+    <nav class="nav-extended cyan darken-1">
+      <div class="nav-content">
+        <router-link to="">
+          <span class="nav-title">Damage Tracker</span>
+        </router-link>
+        <a href="" class="btn-floating btn-large halfway-fab deep-purple">
+          <router-link to="">
+            <i class="material-icons">add</i>
+          </router-link>
+        </a>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style>
+.navbar nav {
+  padding: 0 20px;
+}
+</style>
