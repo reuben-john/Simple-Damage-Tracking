e41d18d71b1ff404c41f48d94f23c46385b37182
Reuben John <rjkoppy@gmail.com>
2018-06-07 15:31:25 -0400

    Links now work

--- simple-damages-tracker/src/components/Navbar.vue
+++ simple-damages-tracker/src/components/Navbar.vue
@@ -2,11 +2,11 @@
   <div class="navbar">
     <nav class="nav-extended cyan darken-1">
       <div class="nav-content">
-        <router-link to="">
+        <router-link :to="{ name: 'Index' }">
           <span class="nav-title">Damage Tracker</span>
         </router-link>
         <a href="" class="btn-floating btn-large halfway-fab deep-purple">
-          <router-link to="">
+          <router-link :to="{ name: 'AddDamages' }">
             <i class="material-icons">add</i>
           </router-link>
         </a>
