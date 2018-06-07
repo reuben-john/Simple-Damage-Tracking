1fd6084158931ee4fdb1349402d11419cba80325
Reuben John <rjkoppy@gmail.com>
2018-06-07 15:32:09 -0400

    Pull total loss data from firestore and display it

--- simple-damages-tracker/src/components/Index.vue
+++ simple-damages-tracker/src/components/Index.vue
@@ -1,20 +1,36 @@
 <template>
   <div class="index container">
-    <h2>Index</h2>
+    <div class="card" v-for="(value, key, index) in totalDamages" :key="index">
+      <div class="card-content">
+        <h5>Total {{key | capitalize}} Damages</h5>
+        <p class="loss">${{ value }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import db from '@/firebase/init'
+
 export default {
   name: 'Index',
   data() {
     return {
-      totalDamages: {
-        shipping: '',
-        product: '',
-        warehouse: ''
-      }
+      totalDamages: null
     }
+  },
+  created() {
+    // fetch data from firestore
+    db
+      .collection('appData')
+      .doc('totalLosses')
+      .get()
+      .then(totals => {
+        this.totalDamages = totals.data()
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
 }
 </script>
@@ -22,9 +38,13 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .index {
-  margin: 20px auto;
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 30px;
+  margin-top: 60px;
 }
-.collection {
-  margin: 0 auto;
+.loss {
+  font-size: 2em;
+  color: red;
 }
 </style>
