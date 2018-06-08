9ad58ecee8cc3b25c90f7390a10624f62f061946
Reuben <rjkoppy@gmail.com>
2018-06-07 20:55:57 -0400

    Add colors to navbar

--- simple-damages-tracker/src/components/Navbar.vue
+++ simple-damages-tracker/src/components/Navbar.vue
@@ -4,13 +4,17 @@
     <nav class="navbar" role="navigation" aria-label="main navigation">
       <div class="navbar-brand">
         <router-link :to="{ name: 'Index' }">
-          <a class="navbar-item"  color="white--text">Damage Tracker</a>
+          <a class="navbar-item">Damage Tracker</a>
         </router-link>
       </div>
       <div class="navbar-menu">
         <div class="navbar-end">
           <router-link :to="{ name: 'AddDamages' }">
-            <h3>Add</h3>
+            <button class="button is-large is-primary">
+            <span class="icon is-large">
+              <i class="mdi mdi-plus mdi-48px"></i>  <!-- bell -->
+            </span>
+            </button>
           </router-link>
         </div>
       </div>
@@ -30,5 +34,13 @@ export default {
 <style>
 .nav-bar nav {
   padding: 10px 20px;
+  background-color: #1976d2;
+}
+.nav-bar button {
+  border-radius: 50px;
+}
+.nav-bar .navbar-brand a {
+  color: white;
+  font-size: 1.4em;
 }
 </style>
