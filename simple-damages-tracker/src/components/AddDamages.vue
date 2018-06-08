47f9da6dd67f2cc64cc9cb8bd9b961ff6dde78a9
Reuben John <rjkoppy@gmail.com>
2018-06-08 15:33:07 -0400

    Pull product costs from firestore

--- simple-damages-tracker/src/components/AddDamages.vue
+++ simple-damages-tracker/src/components/AddDamages.vue
@@ -2,28 +2,35 @@
   <div class="add-damages">
     <v-container>
       <v-layout row wrap>
-      <v-flex xs12 sm6 offset-sm3>
-        <v-card width="600">
-          <v-card-title primary-title>
-            <v-flex text-xs-center>Add Damage</v-flex>
-          </v-card-title>
-          <v-card-text v-if="damageReasons">
-            <v-select
-          :items="orders"
-          v-model="selected"
-          label="Select"
-          single-line
-        ></v-select>
-        <h2>{{ selected }}</h2>
-          </v-card-text>
-        </v-card>
-
-
-      </v-flex>
-    </v-layout>
-
+        <v-flex xs12 sm6 offset-sm3>
+          <v-card width="600">
+            <v-card-title primary-title>
+              <v-flex text-xs-center>
+                <h1>Add Damage Report</h1>
+              </v-flex>
+            </v-card-title>
+            <v-card-text v-if="damageReasons">
+              <v-radio-group v-model="damageDept">
+                <v-radio
+                  v-for="(value, key) in damageReasons"
+                  :key="key"
+                  :label="`Add new ${key} damage report`"
+                  :value="key"
+                ></v-radio>
+                <h2>{{damageDept}}</h2>
+              </v-radio-group>
+              <v-select v-if="damageDept == 'order'"
+                :items="damageReasons.order.type"
+                v-model="damageReport.reasonLost"
+                label="Damage Type"
+                single-line
+              ></v-select>
+              <p>{{ damageReport }}</p>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
     </v-container>
-
   </div>
 </template>
 
@@ -34,25 +41,36 @@ export default {
   name: 'AddDamages',
   data() {
     return {
-      departments: null,
+      appData: {},
+      productCosts: null,
       damageReasons: null,
       damageDept: null,
-      selected: null,
-      warehouse: ['Bad Product', 'Bad Box'],
-      orders: ['Bad Product', 'Returned Product', 'Damaged in shipping']
+      damageReport: {}
     }
   },
   method: {
     addDamages() {}
   },
   created() {
-    // fetch data from firestore
+    // Get damage reasons from firestore
     db
       .collection('appData')
       .doc('damageReasons')
       .get()
-      .then(reasons => {
-        this.damageReasons = reasons.data()
+      .then(doc => {
+        this.damageReasons = doc.data()
+      })
+      .catch(err => {
+        console.log(err)
+      })
+
+    // Get product costs from firestore
+    db
+      .collection('appData')
+      .doc('productCosts')
+      .get()
+      .then(doc => {
+        this.productCosts = doc.data()
       })
       .catch(err => {
         console.log(err)
