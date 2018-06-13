bda292cc375de3cddf02ce8169cfe2bc71665c63
Reuben John <rjkoppy@gmail.com>
2018-06-13 11:33:47 -0400

    Now pulls and displays data for warehouse damages

--- simple-damages-tracker/src/components/ViewWarehouseDamages.vue
+++ simple-damages-tracker/src/components/ViewWarehouseDamages.vue
@@ -1,42 +1,78 @@
 <template>
 <div class="view-warehouse-damages">
-  <h2>View Warehouse Damages</h2>
+
+  <v-container text-xs-center>
+      <v-layout row wrap align-center>
+        <v-flex>
+          <v-card>
+            <v-card-title primary-title>
+              <v-flex>
+                <h1>View warehouse Damages</h1>
+              </v-flex>
+            </v-card-title>
+            <v-card-text v-if="warehouseDamages">
+              <v-data-table
+              :headers="warehouseHeaders"
+              :items="warehouseDamages"
+              hide-actions
+              class="elevation-1"
+              >
+                <template slot="items" slot-scope="props">
+                  <td class="text-xs-left">{{ props.item.timestamp }}</td>
+                  <td class="text-xs-left">{{ props.item.itemType }}</td>
+                  <td class="text-xs-left">{{ props.item.itemsLost }}</td>
+                  <td class="text-xs-left">{{ props.item.reasonLost }}</td>
+                </template>
+              </v-data-table>
+            </v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
 </div>
 </template>
 
 <script>
+import db from '@/firebase/init'
+import moment from 'moment'
+
 export default {
   name: 'ViewWarehouseDamages',
   data() {
     return {
-      warehouseDamages: [
-        {
-          date: '01/05/2018',
-          numberLost: '1',
-          cost: '20.00',
-          itemType: 'food',
-          lossType: 'Item Loss'
-        },
-        {
-          date: '02/05/2018',
-          numberLost: '5',
-          itemCost: '30.00',
-          itemType: 'general',
-          lossType: 'Item Loss'
-        },
-        {
-          date: '02/05/2018',
-          numberLost: '1',
-          cost: '40.00',
-          itemType: 'beauty',
-          lossType: 'Box Loss'
-        }
+      warehouseDamages: null,
+      warehouseHeaders: [
+        { text: 'Date', value: 'timestamp' },
+        { text: 'Item Type', value: 'itemType' },
+        { text: 'Items Lost', value: 'itemsLost' },
+        { text: 'Reason Lost', value: 'reasonLost' }
       ]
     }
+  },
+  created() {
+    let damagesRef = db.collection('damages').orderBy('timestamp')
+    // fetch data from firestore
+    let querty = damagesRef
+      .where('damageDept', '==', 'warehouse')
+      .get()
+      .then(snapshot => {
+        // Make warehouse an object to add the damage reports to it
+        this.warehouseDamages = []
+
+        snapshot.forEach(doc => {
+          let report = doc.data()
+          report.id = doc.id
+          report.value = false
+          report.timestamp = moment(report.timestamp).format('LL')
+          this.warehouseDamages.push(report)
+        })
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
 }
 </script>
 
 <style>
 </style>
-
