f06f67f6ea100e4c9379f93101ec329ec87d92d6
Reuben John <rjkoppy@gmail.com>
2018-06-13 09:05:19 -0400

    Pull order damages from firestore

--- simple-damages-tracker/src/components/ViewOrderDamages.vue
+++ simple-damages-tracker/src/components/ViewOrderDamages.vue
@@ -5,46 +5,33 @@
 </template>
 
 <script>
+import db from '@/firebase/init'
+
 export default {
   name: 'ViewOrderDamages',
   data() {
     return {
-      orderDamages: [
-        {
-          date: '01/05/2018',
-          orderNumber: '5',
-          orderCost: '5.99',
-          shippingCost: '2.61',
-          shippingLoss: '2.61',
-          itemType: 'beauty',
-          itemCost: '1',
-          itemsLost: '2',
-          lossType: 'Bad Product'
-        },
-        {
-          date: '02/05/2018',
-          orderNumber: '25',
-          orderCost: '15.99',
-          shippingCost: '5.61',
-          shippingLoss: '3.61',
-          itemType: 'clothing',
-          itemCost: '4',
-          itemsLost: '0',
-          lossType: 'Returned Product'
-        },
-        {
-          date: '02/05/2018',
-          orderNumber: '15',
-          orderCost: '8.99',
-          shippingCost: '2.61',
-          shippingLoss: '2.61',
-          itemType: 'makeup',
-          itemCost: '0.3',
-          itemsLost: '1',
-          lossType: 'Returned Product'
-        }
-      ]
+      orderDamages: null
     }
+        },
+  created() {
+    let damagesRef = db.collection('damages').orderBy('timestamp')
+    // fetch data from firestore
+    let querty = damagesRef
+      .where('damageDept', '==', 'order')
+      .get()
+      .then(snapshot => {
+        // Make orderDamages an object to add the damage reports to it
+        this.orderDamages = {}
+
+        snapshot.forEach(doc => {
+          this.orderDamages[doc.id] = doc.data()
+          console.log(this.orderDamages[doc.id])
+        })
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
 }
 </script>
