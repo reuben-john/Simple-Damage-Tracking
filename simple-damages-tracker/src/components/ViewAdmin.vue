fad655773b6e7e63194da19603cd9e74a7a092d3
Reuben John <rjkoppy@gmail.com>
2018-06-13 17:19:42 -0400

    Set up basic template for admin options page

--- simple-damages-tracker/src/components/ViewAdmin.vue
+++ simple-damages-tracker/src/components/ViewAdmin.vue
@@ -1,6 +1,28 @@
 <template>
-  <v-container>
+  <v-container class="view-admin" text-xs-center fill-height>
+    <v-layout row wrap align-center justify-center>
+      <v-flex xs12 sm10>
+        <v-card>
+          <v-card-title primary-title>
+            <v-flex>
+              <h2>Admin Options</h2>
+            </v-flex>
+          </v-card-title>
+          <v-card-text>
+            <v-layout row wrap>
+              <v-flex xs12 sm6>
+              <v-btn color="purple darken-1 white--text" >View Damage Reasons</v-btn>
+            </v-flex>
+            <v-flex xs12 sm6>
+              <v-btn color="purple darken-1 white--text">View Product Costs</v-btn>
+            </v-flex>
+            </v-layout>
 
+          </v-card-text>
+        </v-card>
+      </v-flex>
+
+    </v-layout>
   </v-container>
 </template>
 
