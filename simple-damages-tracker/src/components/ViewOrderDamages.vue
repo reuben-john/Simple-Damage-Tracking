944cff743613fe3fb569ffa2f7bd4b55849d290b
Reuben John <rjkoppy@gmail.com>
2018-06-13 09:55:08 -0400

    Table now uses live data

--- simple-damages-tracker/src/components/ViewOrderDamages.vue
+++ simple-damages-tracker/src/components/ViewOrderDamages.vue
@@ -3,7 +3,7 @@
 
   <v-container text-xs-center>
       <v-layout row wrap align-center>
-        <v-flex xs12 sm6 offset-sm1 offset-md2>
+        <v-flex>
           <v-card>
             <v-card-title primary-title>
               <v-flex>
@@ -12,16 +12,16 @@
             </v-card-title>
             <v-card-text v-if="orderDamages">
               <v-data-table
-              :headers="damageHeaders"
-              :items="orderDamageTable"
+              :headers="orderHeaders"
+              :items="orderDamages"
               hide-actions
               class="elevation-1"
               >
                 <template slot="items" slot-scope="props">
-                  <td>{{ props.item.timestamp }}</td>
-                  <td class="text-xs-right">{{ props.item.orderNumber }}</td>
-                  <td class="text-xs-right">{{ props.item.orderTotal }}</td>
-                  <td class="text-xs-right">{{ props.item.reasonLost }}</td>
+                  <td class="text-xs-left">{{ props.item.timestamp }}</td>
+                  <td class="text-xs-left">{{ props.item.orderNumber }}</td>
+                  <td class="text-xs-left">{{ props.item.orderTotal }}</td>
+                  <td class="text-xs-left">{{ props.item.reasonLost }}</td>
                 </template>
               </v-data-table>
               <v-flex
@@ -48,50 +48,11 @@ export default {
   data() {
     return {
       orderDamages: null,
-      damageHeaders: [
+      orderHeaders: [
         { text: 'Date', value: 'timestamp' },
         { text: 'Ebay Order', value: 'orderNumber' },
         { text: 'Order Total', value: 'orderTotal' },
         { text: 'Reason Lost', value: 'reasonLost' }
-      ],
-      orderDamageTable: [
-        {
-          damageDept: 'order',
-          itemCost: 0.25,
-          itemType: 'makeup',
-          itemsLost: '123',
-          orderNumber: '123',
-          orderTotal: '123',
-          reasonLost: 'Returned Product',
-          shippingCost: '123',
-          shippingLost: '123',
-          timestamp: 1528846945595,
-          value: false
-        },
-        {
-          damageDept: 'order',
-          itemCost: 0.25,
-          itemType: 'makeup',
-          itemsLost: '123',
-          orderNumber: '123',
-          orderTotal: '123',
-          reasonLost: 'Returned Product',
-          shippingCost: '123',
-          shippingLost: '123',
-          timestamp: 1528846945595,
-          value: false
-        }
-      ],
-      desserts: [
-        {
-          value: false,
-          name: 'Frozen Yogurt',
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-          iron: '1%'
-        }
       ]
     }
   },
@@ -103,11 +64,14 @@ export default {
       .get()
       .then(snapshot => {
         // Make orderDamages an object to add the damage reports to it
-        this.orderDamages = {}
+        this.orderDamages = []
 
         snapshot.forEach(doc => {
-          this.orderDamages[doc.id] = doc.data()
-          this.orderDamages[doc.id].value = false
+          let report = doc.data()
+          report.id = doc.id
+          report.value = false
+          report.timestamp = moment(report.timestamp).format('LL')
+          this.orderDamages.push(report)
         })
       })
       .catch(err => {
