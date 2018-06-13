3f8dabba568a04c03bc83a51ab4a376936586a11
Reuben John <rjkoppy@gmail.com>
2018-06-13 17:02:32 -0400

    Fix layout issues

--- simple-damages-tracker/src/components/Index.vue
+++ simple-damages-tracker/src/components/Index.vue
@@ -3,30 +3,29 @@
 <template>
 
     <v-container  class="index" text-xs-center fill-height v-if="totalDamages">
-      <v-layout row wrap align-center>
-        <v-flex xs12 mb-3>
-          <v-card>
-            <router-link :to="{ name: totalDamages.order.slug}">
+      <v-layout row wrap align-center justify-center>
+        <v-flex xs12 sm10>
+          <v-card class="elevation-12">
+            <v-card class="elevation-5">
+              <router-link :to="{ name: totalDamages.order.slug}">
               <h2>Total Order Losses</h2>
               <p class="loss loss-money">${{ totalDamages.order.total }}</p>
               <v-layout row wrap>
-                <v-flex align-start text-xs-left offset-xs1 class="loss">
+                <v-flex class="loss">
                   Item Losses:  <span class="loss-money">${{ totalDamages.order.itemTotal }}</span>
                 </v-flex>
-                <v-flex align-start text-xs-left class="loss">
+                <v-flex class="loss">
                   Shipping Losses:  <span class="loss-money">${{ totalDamages.order.shipTotal }}</span>
                 </v-flex>
               </v-layout>
             </router-link>
-          </v-card>
-
-        </v-flex>
-        <v-flex xs12>
-          <v-card>
+            </v-card>
+            <v-card class="elevation-3">
             <router-link :to="{ name: totalDamages.warehouse.slug}">
               <h2>Total Warehouse Losses</h2>
               <p class="loss loss-money">${{ totalDamages.warehouse.total }}</p>
             </router-link>
+          </v-card>
           </v-card>
         </v-flex>
       </v-layout>
@@ -60,17 +59,8 @@ export default {
 </script>
 
 <style>
-.index {
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-gap: 60px;
-  margin-top: 60px;
-}
 .index h2 {
-  font-size: 2em;
-}
-.index .card-content {
-  text-align: center;
+  font-size: 2.2em;
 }
 .loss {
   font-size: 2em;
