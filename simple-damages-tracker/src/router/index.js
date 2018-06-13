8420247b1f55f7f7d8a7ff561750fd012f11ac65
Reuben John <rjkoppy@gmail.com>
2018-06-13 11:19:57 -0400

    Remove unused routes

--- simple-damages-tracker/src/router/index.js
+++ simple-damages-tracker/src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Index from '@/components/Index'
 import AddDamages from '@/components/AddDamages'
-import ViewDamages from '@/components/ViewDamages'
 import ViewOrderDamages from '@/components/ViewOrderDamages'
 import ViewWarehouseDamages from '@/components/ViewWarehouseDamages'
 
@@ -19,11 +18,6 @@ export default new Router({
       path: '/add-damages',
       name: 'AddDamages',
       component: AddDamages
-    },
-    {
-      path: '/view-damages',
-      name: 'ViewDamages',
-      component: ViewDamages
     },
     {
       path: '/order-damages',
