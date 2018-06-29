4d8de9e1efbc9f7c4c748a457bf170aaddbaf580
Reuben <rjkoppy@gmail.com>
2018-06-29 08:39:22 -0400

    Show correct info when button is clicked after db update

--- Vue Version/src/components/EditDamageReasons.vue
+++ Vue Version/src/components/EditDamageReasons.vue
@@ -4,7 +4,7 @@
           <v-card-title primary-title>
             <v-flex>
               <h2>Damage Reasons</h2>
-              <li v-for="(reason, index) in damageReasons.order.type" :key="index"> {{reason}}</li>
+              <li v-for="(reason, index) in damageReasons" :key="index"> {{reason.department}}: {{reason.reasons}}</li>
             </v-flex>
           </v-card-title>
           <v-card-text>
