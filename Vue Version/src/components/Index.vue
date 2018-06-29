24cde201496fd4148364b5bba30af57ba890696f
Reuben <rjkoppy@gmail.com>
2018-06-29 16:40:22 -0400

    Add todo

--- Vue Version/src/components/Index.vue
+++ Vue Version/src/components/Index.vue
@@ -3,6 +3,7 @@
 // TODO add button for View Damages
 // TODO make hover/mouseover on the loss sections do something to indicate it is a button/clickable
 // TODO Add updated message when saving edited items
+// TODO add form verification to verify number fields
 <template>
 
     <v-container  class="index" text-xs-center fill-height v-if="dataLoaded">
