fdf28b076111d18a7e88731989148b094c26773e
Reuben <rjkoppy@gmail.com>
2018-06-30 11:34:54 -0400

    Make forms submit when Enter key is pressed

--- Vue Version/src/components/admin/EditProductCosts.vue
+++ Vue Version/src/components/admin/EditProductCosts.vue
@@ -3,80 +3,84 @@
           <v-card-text>
             <v-dialog v-model="editDialog" max-width="500px">
               <v-card>
-                <v-card-title>
-                  <span class="headline"> Edit Item </span>
-                </v-card-title>
-                <v-card-text>
-                  <v-container grid-list-md>
-                    <v-layout wrap>
-                      <v-flex xs12 sm6 md4>
-                        <v-text-field
-                          v-model="item.name"
-                          label="Category Name"
-                          type="text"
-                        ></v-text-field>
-                      </v-flex>
-                      <v-flex xs12 sm6 md4>
-                        <v-text-field
-                          v-model="item.boxCost"
-                          label="Box Cost"
-                          type="number"
-                        ></v-text-field>
-                      </v-flex>
-                      <v-flex xs12 sm6 md4>
-                        <v-text-field
-                          v-model="item.itemCost"
-                          label="Item Cost"
-                          type="number"
-                        ></v-text-field>
-                      </v-flex>
-                    </v-layout>
-                  </v-container>
-                </v-card-text>
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="blue darken-1" flat @click.native="close">Cancel</v-btn>
-                  <v-btn color="blue darken-1" flat @click.native="save">Save</v-btn>
-                </v-card-actions>
+                <v-form @submit.prevent="save">
+                  <v-card-title>
+                    <span class="headline"> Edit Item </span>
+                  </v-card-title>
+                  <v-card-text>
+                    <v-container grid-list-md>
+                      <v-layout wrap>
+                        <v-flex xs12 sm6 md4>
+                          <v-text-field
+                            v-model="item.name"
+                            label="Category Name"
+                            type="text"
+                          ></v-text-field>
+                        </v-flex>
+                        <v-flex xs12 sm6 md4>
+                          <v-text-field
+                            v-model="item.boxCost"
+                            label="Box Cost"
+                            type="number"
+                          ></v-text-field>
+                        </v-flex>
+                        <v-flex xs12 sm6 md4>
+                          <v-text-field
+                            v-model="item.itemCost"
+                            label="Item Cost"
+                            type="number"
+                          ></v-text-field>
+                        </v-flex>
+                      </v-layout>
+                    </v-container>
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue darken-1" flat @click.native="close">Cancel</v-btn>
+                    <v-btn color="blue darken-1" flat type="submit">Save</v-btn>
+                  </v-card-actions>
+                </v-form>
               </v-card>
             </v-dialog>
             <v-dialog v-model="addDialog" max-width="500px">
               <v-card>
-                <v-card-title>
-                  <span class="headline"> Edit Item </span>
-                </v-card-title>
-                <v-card-text>
-                  <v-container grid-list-md>
-                    <v-layout wrap>
-                      <v-flex xs12 sm6 md4>
-                        <v-text-field
-                          v-model="item.name"
-                          label="Category Name"
-                          type="text"
-                        ></v-text-field>
-                      </v-flex>
-                      <v-flex xs12 sm6 md4>
-                        <v-text-field
-                          v-model="item.boxCost"
-                          label="Box Cost"
-                          type="number"
-                        ></v-text-field>
-                      </v-flex>
-                      <v-flex xs12 sm6 md4>
-                        <v-text-field
-                          v-model="item.itemCost"
-                          label="Item Cost"
-                          type="number"
-                        ></v-text-field>
-                      </v-flex>
-                    </v-layout>
-                  </v-container>
-                </v-card-text>
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="blue darken-1" flat @click.native="close">Cancel</v-btn>
-                  <v-btn color="blue darken-1" flat @click.native="save">Save</v-btn>
-                </v-card-actions>
+                <v-form @submit.prevent="save">
+                  <v-card-title>
+                    <span class="headline"> Edit Item </span>
+                  </v-card-title>
+                  <v-card-text>
+                    <v-container grid-list-md>
+                      <v-layout wrap>
+                        <v-flex xs12 sm6 md4>
+                          <v-text-field
+                            v-model="item.name"
+                            label="Category Name"
+                            type="text"
+                          ></v-text-field>
+                        </v-flex>
+                        <v-flex xs12 sm6 md4>
+                          <v-text-field
+                            v-model="item.boxCost"
+                            label="Box Cost"
+                            type="number"
+                          ></v-text-field>
+                        </v-flex>
+                        <v-flex xs12 sm6 md4>
+                          <v-text-field
+                            v-model="item.itemCost"
+                            label="Item Cost"
+                            type="number"
+                          ></v-text-field>
+                        </v-flex>
+                      </v-layout>
+                    </v-container>
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="blue darken-1" flat @click.native="close">Cancel</v-btn>
+                    <v-btn color="blue darken-1" flat type="submit">Save</v-btn>
+                  </v-card-actions>
+                </v-form>
               </v-card>
             </v-dialog>
             <v-layout row wrap justify-space-around>
