ca5bb8e5e83bbc221f88acb6134522c266912e52
Reuben <rjkoppy@gmail.com>
2018-06-30 11:11:42 -0400

    Create Add New Type function to add new product type

--- Vue Version/src/components/admin/EditProductCosts.vue
+++ Vue Version/src/components/admin/EditProductCosts.vue
@@ -1,7 +1,7 @@
 <template>
         <v-card>
           <v-card-text>
-            <v-dialog v-model="dialog" max-width="500px">
+            <v-dialog v-model="editDialog" max-width="500px">
               <v-card>
                 <v-card-title>
                   <span class="headline"> Edit Item </span>
@@ -11,21 +11,60 @@
                     <v-layout wrap>
                       <v-flex xs12 sm6 md4>
                         <v-text-field
-                          v-model="editedItem.name"
+                          v-model="item.name"
                           label="Category Name"
                           type="text"
                         ></v-text-field>
                       </v-flex>
                       <v-flex xs12 sm6 md4>
                         <v-text-field
-                          v-model="editedItem.boxCost"
+                          v-model="item.boxCost"
                           label="Box Cost"
                           type="number"
                         ></v-text-field>
                       </v-flex>
                       <v-flex xs12 sm6 md4>
                         <v-text-field
-                          v-model="editedItem.itemCost"
+                          v-model="item.itemCost"
+                          label="Item Cost"
+                          type="number"
+                        ></v-text-field>
+                      </v-flex>
+                    </v-layout>
+                  </v-container>
+                </v-card-text>
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn color="blue darken-1" flat @click.native="close">Cancel</v-btn>
+                  <v-btn color="blue darken-1" flat @click.native="save">Save</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-dialog>
+            <v-dialog v-model="addDialog" max-width="500px">
+              <v-card>
+                <v-card-title>
+                  <span class="headline"> Edit Item </span>
+                </v-card-title>
+                <v-card-text>
+                  <v-container grid-list-md>
+                    <v-layout wrap>
+                      <v-flex xs12 sm6 md4>
+                        <v-text-field
+                          v-model="item.name"
+                          label="Category Name"
+                          type="text"
+                        ></v-text-field>
+                      </v-flex>
+                      <v-flex xs12 sm6 md4>
+                        <v-text-field
+                          v-model="item.boxCost"
+                          label="Box Cost"
+                          type="number"
+                        ></v-text-field>
+                      </v-flex>
+                      <v-flex xs12 sm6 md4>
+                        <v-text-field
+                          v-model="item.itemCost"
                           label="Item Cost"
                           type="number"
                         ></v-text-field>
@@ -65,6 +104,13 @@
               </v-flex>
             </v-layout>
           </v-card-text>
+          <v-card-actions>
+            <v-layout row wrap justify-center>
+              <v-flex>
+                <v-btn color="success" @click="addItem">Add New Type</v-btn>
+              </v-flex>
+            </v-layout>
+          </v-card-actions>
         </v-card>
 
 </template>
@@ -82,25 +128,31 @@ export default {
         { text: 'Box Cost', value: 'boxCost' },
         { text: 'Item Cost', value: 'itemCost' }
       ],
-      dialog: false,
+      editDialog: false,
+      addDialog: false,
       editedIndex: -1,
-      editedItem: {
+      item: {
         name: '',
         boxCost: 0,
         itemCost: 0
       },
       defaultItem: {
-        name: '',
-        boxCost: 0,
-        itemCost: 0
+        name: 'test',
+        boxCost: 1,
+        itemCost: 1
       }
     }
   },
   methods: {
+    addItem(item) {
+      this.editedIndex = this.productCosts.indexOf(item)
+      this.item = Object.assign({}, item)
+      this.addDialog = true
+    },
     editItem(item) {
       this.editedIndex = this.productCosts.indexOf(item)
-      this.editedItem = Object.assign({}, item)
-      this.dialog = true
+      this.item = Object.assign({}, item)
+      this.editDialog = true
     },
     deleteFromDB(itemId) {
       db
@@ -112,33 +164,37 @@ export default {
     },
     deleteItem(item) {
       const index = this.productCosts.indexOf(item)
-      console.log(item)
       // Convert name to lowercase as doc id is lowercase category name
       let id = item.name.toLowerCase()
-      console.log(id)
 
       confirm('Are you sure you want to delete this item?') &&
         (this.productCosts.splice(index, 1), this.deleteFromDB(id))
     },
     close() {
-      this.dialog = false
+      this.editDialog = false
+      this.addDialog = false
       setTimeout(() => {
-        this.editedItem = Object.assign({}, this.defaultItem)
+        this.item = Object.assign({}, this.defaultItem)
         this.editedIndex = -1
       }, 300)
     },
     save() {
+      let id = this.item.name.toLowerCase()
+      // Check if adding or updating item
       if (this.editedIndex > -1) {
-        Object.assign(this.productCosts[this.editedIndex], this.editedItem)
+        Object.assign(this.productCosts[this.editedIndex], this.item)
         // Convert name to lowercase as doc id is lowercase category name
-        let id = item.name.toLowerCase()
 
-        this.updateItem(id)
+        if (this.editDialog == true) {
+          this.updateItem(id)
+        }
+      } else if (this.addDialog == true) {
+        this.addNewItem(id)
       }
       this.close()
     },
     convertNumbers() {
-      let report = Object.assign(this.editedItem)
+      let report = Object.assign(this.item)
 
       // Convert string to numbers for different fields before adding to database
       report = Object.assign(report, {
@@ -149,25 +205,42 @@ export default {
     },
     updateItem(itemId) {
       // Convert edited numbers to fields
-      let report = Object.assign(this.editedItem)
-      this.editedItem = this.convertNumbers(report)
+      let report = Object.assign(this.item)
+      this.item = this.convertNumbers(report)
 
       let ref = db.collection('productCosts').doc(itemId)
       // fetch data from firestore
       let querty = ref
         .update({
-          name: this.editedItem.name,
-          boxCost: this.editedItem.boxCost,
-          itemCost: this.editedItem.itemCost
+          name: this.item.name,
+          boxCost: this.item.boxCost,
+          itemCost: this.item.itemCost
         })
         .then(console.log('Updated'))
+        .catch(err => {
+          console.log(err)
+        })
+    },
+    addNewItem(itemId) {
+      // Convert edited numbers to fields
+      let report = Object.assign(this.item)
+      this.item = this.convertNumbers(report)
+
+      let ref = db.collection('productCosts').doc(itemId)
+      // fetch data from firestore
+      let querty = ref
+        .set(this.item)
+        .then(console.log('Added'))
         .catch(err => {
           console.log(err)
         })
     }
   },
   watch: {
-    dialog(val) {
+    editDialog(val) {
+      val || this.close()
+    },
+    addDialog(val) {
       val || this.close()
     }
   }
