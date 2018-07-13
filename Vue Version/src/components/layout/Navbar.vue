c72243a01943b0af02922e6359d625a0b88ef95a
Reuben <rjkoppy@gmail.com>
2018-07-13 12:50:01 -0400

    Change navbar styling

--- Vue Version/src/components/layout/Navbar.vue
+++ Vue Version/src/components/layout/Navbar.vue
@@ -3,15 +3,15 @@
       <v-toolbar color="primary">
       <v-toolbar-title>
         <router-link :to="{ name: 'Index' }">
-          <span class="white--text">Damage Tracker</span>
+          <span class="white--text ml-2">Damage Tracker</span>
         </router-link>
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items>
         <v-btn v-if="user" flat class="white--text" :to="{ name: 'ViewAdmin' }">Admin</v-btn>
-        <v-btn v-if="!user" flat class="white--text" :to="{ name: 'Login' }">Login</v-btn>
-        <v-btn v-if="user" flat class="white--text" @click="logout">Logout</v-btn>
-        <v-btn class="add" fab top right large color="pink white--text" :to="{ name: 'AddDamages' }">
+        <v-btn v-if="!user" flat class="white--text mr-4" :to="{ name: 'Login' }">Login</v-btn>
+        <v-btn v-if="user" flat class="white--text mr-4" @click="logout">Logout</v-btn>
+        <v-btn class="add" fab top right large color="pink white--text mt-2" :to="{ name: 'AddDamages' }">
           <v-icon>add</v-icon>
         </v-btn>
       </v-toolbar-items>
@@ -55,8 +55,5 @@ export default {
 <style>
 .nav-bar {
   margin-bottom: 30px;
-}
-.nav-bar .add {
-  margin-top: 1em;
 }
 </style>
